84b21d16f4c02471de6390f09186e508bbf9ba96
Celeste Gateley <celeste.gateley@gmail.com>
2023-10-25 18:59:36 +0200

    Installed boostrap dependency

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,18 @@
 /**
+ * Setup for bootstrap
+ */
+import lodash from 'lodash';
+window._ = lodash;
+
+import jquery from 'jquery';
+window.$ = jquery;
+
+import * as Popper from '@popperjs/core';
+window.Popper = Popper;
+
+import 'bootstrap';
+
+/**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
